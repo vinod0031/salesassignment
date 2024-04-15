3fe7d94779614e06f57f72c8893e7f71ec8ea69c
vinod0031 <kvinodkumar0031@gmail.com>
2024-04-15 22:19:46 +0530

    init

---
+++ sales/src/components/MenBags/index.js
@@ -0,0 +1,45 @@
+import React ,{useState,useEffect}from "react";
+import "./index.css";
+import axios from "axios";
+
+
+const MenBags =()=>{
+
+
+    const [menbags ,setMenbags]=useState([]);
+
+    useEffect(()=>{
+        fetchdata()
+
+    },[])
+
+    const fetchdata= async()=>{
+
+        try{
+        const response=await axios.get("http://localhost:5000/api/items")
+        setMenbags(response.data);
+    }   catch (error){
+        console.error(error)
+    }
+    }
+
+
+
+
+
+    return (<div>
+        <h1 style={{color:"#383542" ,fontFamily:"cursive"}}>Men's Bags</h1>
+        
+        <ul className="container">
+            {menbags.map(bag=>(
+                <li key={bag._id} className="liststyles">
+                    <img src={bag.images} className="bagsize"/>
+                    <p className="mrp">MRP : <span className="price">{bag.mrp}</span></p>
+                    <p className="mrp">Discount Price :<span className="dprice">{bag.discount}</span></p>
+                </li>
+            ))}
+        </ul>
+    </div>)
+}
+
+export default MenBags;
